fed81d812783eafdbffc1694b9ab191ce0f3c531
michaelchen-lab <michaelchenkaijie2004@gmail.com>
2020-12-01 11:13:00 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['./src/**/*.js', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+        sans: ['Roboto', '"Segoe UI"']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
